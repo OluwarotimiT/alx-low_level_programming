5271e431751aed667c5aefa27e065895ae57c796
OluwarotimiT <timzzy.ra@gmail.com>
2022-07-05 16:55:07 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,10 @@
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	int count = 0;
@@ -19,4 +22,5 @@ int main(void)
 			_putchar(letter);
 		_putchar('\n');
 	}
+	return (0);
 }
