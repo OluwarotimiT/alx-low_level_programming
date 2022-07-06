0ea5c2cd7a76218e44af5733298151f8de54aae0
OluwarotimiT <timzzy.ra@gmail.com>
2022-07-06 10:02:28 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -23,7 +23,6 @@ int main(void)
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 
-
 		fib1 = fib2;
 		fib2 = sum;
 	}
@@ -32,7 +31,6 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-
 
 	for (count = 93; count < 99; count++)
 	{
