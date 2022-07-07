bd51786fffd6d8e3300b579f9635dcaa1b9bc364
OluwarotimiT <timzzy.ra@gmail.com>
2022-07-07 07:31:47 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,8 +1,3 @@
-/*
- * File: 3-print_remaining_days.c
- * Auth: OluwarotimiT
- */
-
 #include "main.h"
 #include <stdio.h>
 
@@ -14,6 +9,7 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
