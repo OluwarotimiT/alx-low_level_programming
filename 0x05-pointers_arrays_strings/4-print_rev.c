0d84397ccef949a6c81932fafeaec54cef61480d
OluwarotimiT <timzzy.ra@gmail.com>
2022-07-12 21:35:59 -0700

    Tasks

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
  */
 void print_rev(char *s)
 {
-  int len = 0;
+	int len = 0;
 
-  while (s[len] != '\0')
-    len++;
+	while (s[len] != '\0')
+		len++;
 
-  while (len)
-    _putchar(s[--len]);
+	while (len)
+		_putchar(s[--len]);
 
-  _putchar('\n');
+	_putchar('\n');
 }
